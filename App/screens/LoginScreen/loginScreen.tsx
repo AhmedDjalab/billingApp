a81306272eb3837492ad96077fbb352da1b26736
AhmedDja <48105133+AhmedLight92@users.noreply.github.com>
2021-08-01 12:17:14 +0100

    Add the Top Input Pannel

--- App/screens/LoginScreen/loginScreen.tsx
+++ App/screens/LoginScreen/loginScreen.tsx
@@ -7,29 +7,88 @@ export default function LoginScreen() {
    let  [password,setPassword] = useState("");
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Ahmed is here </Text>
+        <View style={styles.topLabelsContainer}>
+              <Text style={{color:"black"}}>Add Product</Text>
+              <Text style={styles.stateSelectedText}>State Selected: AL</Text>
+        </View>
+              
             <TextInput
+            style={styles.inputStyle}
                placeholder="Enter Product Label"
+               placeholderTextColor="#CFCFCF"
                onChangeText={text => setUsername(text)}
                value={username}
             />
+             <View style = {styles.horizentalTextInput}>
              <TextInput
-               placeholder="Password" 
+            style={[styles.inputStyle , styles.inputHrizentalStyle]}
+               placeholder="Enter Price"
+               placeholderTextColor="#CFCFCF"
                onChangeText={text => setUsername(text)}
-               value={username}    
-               secureTextEntry
-               textContentType="password"
+               value={username}
             />
+              <TextInput
+            style={[styles.inputStyle , styles.inputHrizentalStyle]}
+               placeholder="Enter Quantity"
+               placeholderTextColor="#CFCFCF"
+               onChangeText={text => setUsername(text)}
+               value={username}
+            />  
+    <Text style={styles.stateSelectedText}>Add To List</Text>
+
+             </View>
+
+            //? building the table compoenents 
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    
+    
+
+    horizentalTextInput : {
+     
+        flexDirection:"row",
+        justifyContent:"space-between"
+        ,alignItems:"center" , 
+        marginTop:10
+
+    } ,
+   stateSelectedText : {
+    color: "#85B6D7" , 
+   } , 
+
+    topLabelsContainer : {
+      
+        flexDirection:"row"
+        ,justifyContent:"space-between" , 
+    
+    
+    },
     container:{
       flex:1,
       width:"100%" , 
-      paddingTop:40 , // this is native margin in andriod phone we will use platform package lateer on 
-    paddingLeft:20,
+     margin:30
+     ,padding:20
+
+    } , 
+    inputStyle:{
+        fontSize:14 , 
+        borderWidth:1,
+        padding:2,
+     borderTopLeftRadius:5,
+     borderTopRightRadius:5,
+     borderBottomLeftRadius:5,
+     borderBottomRightRadius:5,
+        borderColor:"#CFCFCF", 
+
+    } , 
+    inputHrizentalStyle:{
+    
+     width:120
+     
+     
     }
 })
 
